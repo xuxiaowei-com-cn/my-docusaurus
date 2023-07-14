433bf4664bcdd9c396923f06bd0d3d1aa30917da
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-07-14 16:03:39 +0800

    :wrench: title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'My Docusaurus',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -64,9 +64,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'My Docusaurus',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'My Docusaurus Logo',
           src: 'img/logo.svg',
         },
         items: [
